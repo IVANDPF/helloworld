cb1a447c1366840f2531525b04c07ea59ca37788
Oscar Calle <gameplaysoscar@gmail.com>
2022-03-09 20:37:48 -0500

    Agragado validación al formulario con javascript

--- js/script.js
+++ js/script.js
@@ -1,2 +1,34 @@
-var a = 5; 
+function validarForm() {
+	const fullname = document.getElementById('fullname').value;
+	const email = document.getElementById('email').value;
+	const password = document.getElementById('password').value;
 
+	if (fullname.trim().length === 0) {
+		alert('El nombre es obligatorio');
+		return false;
+	}
+
+	if (email.trim().length === 0) {
+		alert('El email es obligatorio');
+		return false;
+	}
+
+	if (!validarEmail(email)) {
+		alert('El email no es valido');
+		return false;
+	}
+
+	if (password.trim().length === 0) {
+		alert('La contraseña es obligatoria');
+		return false;
+	}
+}
+
+function validarEmail(email) {
+	return email.includes('@');
+
+	// Validar con expresiones regulares
+	// const regExp =
+	// 	/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	// return re.test(email.toLowerCase());
+}
