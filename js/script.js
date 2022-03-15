2401bf1eaa978bf97836b597c536a4c57c37d9ea
Ivan <ivanmdelpozof@gmail.com>
2022-03-14 20:51:55 -0500

    position and check box

--- js/script.js
+++ js/script.js
@@ -1,9 +1,23 @@
 function valida_envio(){
+
+    //this.valida_nombre();
+    //this.valida_email();
+    //this.valida_password();
+    this.valida_ifIsChecked();
+
+}
+
+function valida_nombre(){
+
     if (document.fvalida.nombre.value.length==0){
         alert('Favor complete el campo full name')
         return 0;
     } 
-    
+    this.valida_email();
+    this.valida_password();
+}
+
+function valida_email(){
     var testemail = document.fvalida.emailForm.value
     console.log(testemail)
     if(testemail.length <= 0){
@@ -25,12 +39,27 @@ function valida_envio(){
         
     }
 
+}
+
+function valida_password(){
     var pss = document.fvalida.passwordForm.value
     console.log(pss)
     if(pss.length >= 7 && pss.length <= 16){
         alert('password valido')
     } else {
         alert('Ingrese un password valido')
+        return 0;
     }
-
 }
+
+function valida_ifIsChecked(){
+    var check = document.getElementById("terms").checked
+    console.log(check);
+
+    if(check == false){
+        alert('Acepta terminos y condiciones')
+    }else{
+        console.log("usuario registrado");
+    } 
+}
+
